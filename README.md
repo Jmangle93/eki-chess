9e34b1232db16851c5fb7af6c742c039a57df582
John Angle <jmangle93@gmail.com>
2025-02-23 14:51:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chess engine and simple web interface
+This is a project to produce a simple chess engine written in Python. It uses the minimax algorithm with alpha-beta pruning to search the game tree. The engine is able to play against itself or against a human player. It is starting as a command line program but I plan to extend it to include a simple web interface.
+
+## References
+- [Chess Programming Wiki](https://www.chessprogramming.org/Main_Page)
+- [Forsyth-Edwards Notation (FEN)](https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation)
+- [Minimax Algorithm](https://en.wikipedia.org/wiki/Minimax)
+- [Alpha-Beta Pruning](https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning)
+  - [Bartek Spitza Programming a Chess Engine](https://youtu.be/w4FFX_otR-4?si=XVETgP7ImFV3nx-D)
+  - [Sebastian Lague explains minimax with alpha-beta pruning](https://youtu.be/l-hh51ncgDI?si=-Ezoi0e0L3yuVOa7)
+  - [Mk-Chan Python Chess Engine Extensions](https://github.com/Mk-Chan/python-chess-engine-extensions)
+- [PolyGlot Opening Books](https://www.chessprogramming.org/PolyGlot)
+  - [PolyGlot downloads](https://rebel13.nl/download/books.html)
---
+++ play_minimax.py
@@ -0,0 +1,140 @@
+import chess
+
+
+KNIGHT_SQUARES = [
+    -10, -8, -6, -6, -6, -6, -8, -10,
+    -8, -4, 0, 0, 0, 0, -4, -8,
+    -6, 0, 2, 3, 3, 2, 0, -6,
+    -6, 1, 3, 4, 4, 3, 1, -6,
+    -6, 0, 3, 4, 4, 3, 0, -6,
+    -6, 1, 2, 3, 3, 2, 1, -6,
+    -8, -4, 0, 1, 1, 0, -4, -8,
+    -10, -8, -6, -6, -6, -6, -8, -10
+]
+
+BISHOP_SQUARES = [
+    -6, -4, -4, -4, -4, -4, -4, -6,
+    -4, 0, 0, 0, 0, 0, 0, -4,
+    -4, 0, 2, 4, 4, 2, 0, -4,
+    -4, 1, 2, 4, 4, 2, 1, -4,
+    -4, 0, 4, 4, 4, 4, 0, -4,
+    -4, 4, 4, 4, 4, 4, 4, -4,
+    -4, 2, 0, 0, 0, 0, 2, -4,
+    -6, -4, -4, -4, -4, -4, -4, -6
+]
+
+ROOK_SQUARES = [
+    0, 0, 0, 1, 1, 0, 0, 0,
+    -1, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, -1,
+    1, 2, 2, 2, 2, 2, 2, 1,
+    0, 0, 0, 0, 0, 0, 0, 0
+]
+
+QUEEN_SQUARES = [
+    -4, -2, -2, -1, -1, -2, -2, -4,
+    -2, 0, 0, 0, 0, 0, 0, -2,
+    -2, 0, 1, 1, 1, 1, 0, -2,
+    -1, 0, 1, 1, 1, 1, 0, -1,
+    0, 0, 1, 1, 1, 1, 0, -1,
+    -2, 1, 1, 1, 1, 1, 0, -2,
+    -2, 0, 1, 0, 0, 0, 0, -2,
+    -4, -2, -2, -1, -1, -2, -2, -4
+]
+
+def piece_value(piece):
+    if piece.piece_type == chess.PAWN:
+        return 1
+    if piece.piece_type == chess.KNIGHT:
+        return 3
+    if piece.piece_type == chess.BISHOP:
+        return 3
+    if piece.piece_type == chess.ROOK:
+        return 5
+    if piece.piece_type == chess.QUEEN:
+        return 9
+    if piece.piece_type == chess.KING:
+        return 0
+    return 0
+
+def rank_targets(board):
+    move = board.peek()
+    targeted = board.attacks(move.to_square)
+    print(f'Move: {move}; Targeted squares:\n{targeted}')
+    targets = [sq for sq in targeted if board.piece_at(sq) and board.piece_at(sq).color != board.turn]
+    ranked_targets = targets.sort(key=lambda sq: piece_value(board.piece_at(sq)), reverse=True)
+    print(f'Targets: {targets}')
+    print(f'Ranked targets: {ranked_targets}')
+    return ranked_targets
+
+def score_by_material(board):
+    score = 0
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece is not None:
+            allegiance = 1 if piece.color == chess.WHITE else -1
+            score += (piece_value(piece) * allegiance)
+
+    if board.is_check():
+        score += (1 * -allegiance)
+    
+    return score
+
+def minimax(board, depth=2, alpha=-9999, beta=9999):
+    if depth == 0:
+        best_move = [score_by_material(board), board.move_stack[-depth]]
+        
+        return best_move
+    best_move = [alpha, None] if board.turn == chess.WHITE else [beta, None]
+    if len(board.move_stack) > 0:
+        best_move[1] = board.peek()
+    if board.turn == chess.WHITE:
+        for move in board.legal_moves:
+            board.push(move)
+            try_move = minimax(board, depth - 1, alpha, beta)
+            if try_move[0] > best_move[0]:
+                best_move = try_move
+            alpha = max(alpha, best_move[0])
+            board.pop()
+            if beta <= alpha:
+                return best_move
+        return best_move
+    else:
+        for move in board.legal_moves:
+            board.push(move)
+            try_move = minimax(board, depth - 1, alpha, beta)
+            if try_move[0] < best_move[0]:
+                best_move = try_move
+            beta = min(beta, best_move[0])
+            board.pop()
+            if beta <= alpha:
+                return best_move
+        return best_move
+
+board = chess.Board()
+
+while not board.is_game_over():
+    print(board)
+    print("Material score: ", score_by_material(board))
+    num_moves = len(board.move_stack)
+    while len(board.move_stack) == num_moves:
+        if board.turn == chess.WHITE:
+            move = minimax(board.copy(stack=False))
+            print(f'Best move: {move[1]}')
+            board.push(move[1])
+        else:
+            move = input("Enter move: ")
+            try:
+                board.push_uci(move)
+            except chess.IllegalMoveError or ValueError:
+                print(f'{move} is not a legal uci move on the board.')
+            except:
+                print(f'An error occurred while processing {move}.')
+
+print("Game over")
+print("Final board:")
+print(board)
+print("Material score: ", score_by_material(board))
